881ec4ee660c2f5c24a46a7621dacfe0060c02e1
Hiroki Inoue <tyynh.hiroki@gmail.com>
2024-05-06 10:50:15 +1000

    create outline of NexTripSetUpPage.tsx

--- src/components/topPage/NewTripSetUpPage.tsx
+++ src/components/topPage/NewTripSetUpPage.tsx
@@ -1,9 +1,147 @@
-import React from 'react'
+import { Box, Button, TextField } from "@mui/material";
+import React, { useEffect, useRef, useState } from "react";
 
-const NewTripSetUpPage = () => {
+type Props = {
+  setNewTripSetUpPage: React.Dispatch<React.SetStateAction<boolean>>;
+  newTripSetUpPage: boolean;
+};
+
+const NewTripSetUpPage = (props: Props) => {
+  const { setNewTripSetUpPage, newTripSetUpPage } = props;
+  const titleRef = useRef<HTMLInputElement>(null);
+  const yourCurrencyRef = useRef<HTMLInputElement>(null);
+  const budgetRef = useRef<HTMLInputElement>(null);
+  const startDayRef = useRef<HTMLInputElement>(null);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
+
+  const closeNewTripSetUpPage = (): void => {
+    setNewTripSetUpPage(false);
+  };
+
+  const handleCreateNewTrip = () => {
+    const title = titleRef.current?.value;
+    const yourCurrency = yourCurrencyRef.current?.value;
+    const budget = budgetRef.current?.value;
+    const startDay = startDayRef.current?.value;
+
+    console.log(title, yourCurrency, budget, startDay);
+
+    // 入力後にインプットvalueを空にする
+    if (titleRef.current) {
+      titleRef.current.value = "";
+    }
+    if (yourCurrencyRef.current) {
+      yourCurrencyRef.current.value = "";
+    }
+    if (budgetRef.current) {
+      budgetRef.current.value = "";
+    }
+    if (startDayRef.current) {
+      startDayRef.current.value = "";
+    }
+    setButtonDisabled(true);
+    setNewTripSetUpPage(false);
+  };
+
+  const boxDesignBefore =
+    "h-[500px] md:w-[50%] w-[80%] fixed top-12 bottom-0 right-0 left-0 mx-auto bg-orange-100 rounded translate-y-[-800px] duration-1000";
+  const boxDesignAfter =
+    "h-[500px] md:w-[50%] w-[80%] fixed top-12 bottom-0 right-0 left-0 mx-auto bg-orange-100 rounded translate-y-[0px] duration-1000";
+
   return (
-    <div>NewTripSetUpPage</div>
-  )
-}
+    <>
+      <div
+        className={
+          newTripSetUpPage
+            ? "fixed top-0 bottom-0 right-0 left-0 opacity-50 bg-slate-500"
+            : "hide"
+        }
+        onClick={closeNewTripSetUpPage}
+      ></div>
+      <div className={newTripSetUpPage ? boxDesignAfter : boxDesignBefore}>
+        <div className="w-full p-8">
+          <h2 className="text-center text-[36px]">New Trip!!</h2>
+          <div className="w-full mt-4">
+            <TextField
+              sx={{ width: "100%" }}
+              id="outlined-basic"
+              label="タイトル"
+              variant="outlined"
+              inputRef={titleRef}
+              onChange={() =>
+                setButtonDisabled(
+                  !titleRef.current?.value ||
+                    !yourCurrencyRef.current?.value ||
+                    !budgetRef.current?.value ||
+                    !startDayRef.current?.value
+                )
+              }
+            />
+          </div>
+          <div className="w-full mt-4">
+            <TextField
+              sx={{ width: "100%" }}
+              id="outlined-basic"
+              label="自国通貨"
+              variant="outlined"
+              inputRef={yourCurrencyRef}
+              onChange={() =>
+                setButtonDisabled(
+                  !titleRef.current?.value ||
+                    !yourCurrencyRef.current?.value ||
+                    !budgetRef.current?.value ||
+                    !startDayRef.current?.value
+                )
+              }
+            />
+          </div>
+          <div className="w-full mt-4">
+            <TextField
+              sx={{ width: "100%" }}
+              id="outlined-basic"
+              label="予算"
+              variant="outlined"
+              inputRef={budgetRef}
+              onChange={() =>
+                setButtonDisabled(
+                  !titleRef.current?.value ||
+                    !yourCurrencyRef.current?.value ||
+                    !budgetRef.current?.value ||
+                    !startDayRef.current?.value
+                )
+              }
+            />
+          </div>
+          <div className="w-full mt-4">
+            <TextField
+              sx={{ width: "100%" }}
+              id="outlined-basic"
+              label="開始日"
+              variant="outlined"
+              inputRef={startDayRef}
+              onChange={() =>
+                setButtonDisabled(
+                  !titleRef.current?.value ||
+                    !yourCurrencyRef.current?.value ||
+                    !budgetRef.current?.value ||
+                    !startDayRef.current?.value
+                )
+              }
+            />
+          </div>
+          <Button
+            sx={{ width: "100%", height: "48px", marginTop: "36px" }}
+            variant="contained"
+            color="warning"
+            onClick={handleCreateNewTrip}
+            disabled={buttonDisabled}
+          >
+            作成
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default NewTripSetUpPage
+export default NewTripSetUpPage;
--- src/components/topPage/TopPage.tsx
+++ src/components/topPage/TopPage.tsx
@@ -53,96 +53,92 @@ const TopPage = () => {
 
   return (
     <>
-      {newTripSetUpPage ? (
-        <>
-          <NewTripSetUpPage />
-        </>
-      ) : (
-        <>
-          <Header />
-          <div className="h-screen bg-orange-300">
-            <div className="bg-orange-300 py-10 h-auto">
-              <div className="h-full sm:w-[60%] w-[95%] mx-auto">
-                <Box
-                  sx={{
-                    display: "flex",
-                    backgroundColor: "rgb(255 237 213)",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "60px",
-                    borderRadius: "8px",
-                    border: "solid",
-                    borderColor: "rgb(251 146 60)",
-                    cursor: "pointer",
-                    boxShadow: "4px 4px 15px -5px #777777",
-                    "&:hover": {
-                      opacity: "0.7",
-                    },
-                  }}
-                  onClick={() => setNewTripSetUpPage(true)}
-                >
-                  <AddCircleOutlineIcon
-                    style={{ fontSize: "32px", color: "rgb(194 65 12)" }}
-                  />
-                </Box>
-                <div className="mt-8">
-                  <Box
-                    sx={{
-                      display: "flex",
-                      backgroundColor: "rgb(255 237 213)",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      height: "100px",
-                      borderRadius: "8px",
-                      border: "solid 2px",
-                      borderColor: "rgb(251 146 60)",
-                      cursor: "pointer",
-                      marginBottom: "18px",
-                      boxShadow: "4px 4px 15px -5px #777777",
-                      "&:hover": {
-                        opacity: "0.7",
-                      },
-                    }}
-                  >
-                    <div className="h-full w-full">
-                      <h2 className="h-[40%] w-full text-[24px] bg-orange-100 text-center rounded-xl">
-                        ({translatedData[language][0]}){" "}
-                        {translatedData[language][1]}
-                      </h2>
-                      <div className="h-[60%] w-full flex  font-bold">
-                        <div className="h-hull w-[33%] px-2">
-                          <p className="h-[50%] flex items-center justify-center border-b-2 border-orange-400">
-                            {translatedData[language][2]}
-                          </p>
-                          <p className="h-[50%] flex items-center justify-center">
-                            {translatedData[language][3]}
-                          </p>
-                        </div>
-                        <div className="h-hull w-[34%] px-2">
-                          <p className="h-[50%] flex items-center justify-center border-b-2 border-orange-400">
-                            {translatedData[language][4]}
-                          </p>
-                          <p className="h-[50%] flex items-center justify-center">
-                            {translatedData[language][5]}
-                          </p>
-                        </div>
-                        <div className="h-hull w-[33%] px-2">
-                          <p className="h-[50%] flex items-center justify-center border-b-2 border-orange-400">
-                            {translatedData[language][6]}
-                          </p>
-                          <p className="h-[50%] flex items-center justify-center">
-                            {translatedData[language][7]}
-                          </p>
-                        </div>
-                      </div>
+      <Header />
+      <div className="h-screen bg-orange-300">
+        <div className="bg-orange-300 py-10 h-auto">
+          <div className="h-full sm:w-[60%] w-[95%] mx-auto">
+            <Box
+              sx={{
+                display: "flex",
+                backgroundColor: "rgb(255 237 213)",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "60px",
+                borderRadius: "8px",
+                border: "solid",
+                borderColor: "rgb(251 146 60)",
+                cursor: "pointer",
+                boxShadow: "4px 4px 15px -5px #777777",
+                "&:hover": {
+                  opacity: "0.7",
+                },
+              }}
+              onClick={() => setNewTripSetUpPage(true)}
+            >
+              <AddCircleOutlineIcon
+                style={{ fontSize: "32px", color: "rgb(194 65 12)" }}
+              />
+            </Box>
+            <div className="mt-8">
+              <Box
+                sx={{
+                  display: "flex",
+                  backgroundColor: "rgb(255 237 213)",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: "100px",
+                  borderRadius: "8px",
+                  border: "solid 2px",
+                  borderColor: "rgb(251 146 60)",
+                  cursor: "pointer",
+                  marginBottom: "18px",
+                  boxShadow: "4px 4px 15px -5px #777777",
+                  "&:hover": {
+                    opacity: "0.7",
+                  },
+                }}
+              >
+                <div className="h-full w-full">
+                  <h2 className="h-[40%] w-full text-[24px] bg-orange-100 text-center rounded-xl">
+                    ({translatedData[language][0]}){" "}
+                    {translatedData[language][1]}
+                  </h2>
+                  <div className="h-[60%] w-full flex  font-bold">
+                    <div className="h-hull w-[33%] px-2">
+                      <p className="h-[50%] flex items-center justify-center border-b-2 border-orange-400">
+                        {translatedData[language][2]}
+                      </p>
+                      <p className="h-[50%] flex items-center justify-center">
+                        {translatedData[language][3]}
+                      </p>
                     </div>
-                  </Box>
+                    <div className="h-hull w-[34%] px-2">
+                      <p className="h-[50%] flex items-center justify-center border-b-2 border-orange-400">
+                        {translatedData[language][4]}
+                      </p>
+                      <p className="h-[50%] flex items-center justify-center">
+                        {translatedData[language][5]}
+                      </p>
+                    </div>
+                    <div className="h-hull w-[33%] px-2">
+                      <p className="h-[50%] flex items-center justify-center border-b-2 border-orange-400">
+                        {translatedData[language][6]}
+                      </p>
+                      <p className="h-[50%] flex items-center justify-center">
+                        {translatedData[language][7]}
+                      </p>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </Box>
             </div>
           </div>
-        </>
-      )}
+        </div>
+      </div>
+      <NewTripSetUpPage
+        setNewTripSetUpPage={setNewTripSetUpPage}
+        newTripSetUpPage={newTripSetUpPage}
+      />
     </>
   );
 };
