4f565478091c83c7984933d1463ef9698ee35db7
unknown <tyynh.hiroki@gmail.com>
2024-12-26 21:45:21 +1100

    refactoring in LoginPage.tsx

--- src/components/loginPage/LoginPage.tsx
+++ src/components/loginPage/LoginPage.tsx
@@ -31,7 +31,6 @@ function LoginPage() {
   };
 
   useEffect(() => {
-    cleanUpLocalStorageExceptLanguage();
     auth.onAuthStateChanged((loginUser) => {
       if (loginUser) {
         dispatch(
